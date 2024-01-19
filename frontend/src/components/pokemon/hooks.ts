c4d70f07bdb6ee44da92558f861650511e67efda
Denis Kabana <denis.kabana@gmail.com>
2024-01-19 17:26:11 +0100

    Responsive fixes, optimistic ui fixes

--- frontend/src/components/pokemon/hooks.ts
+++ frontend/src/components/pokemon/hooks.ts
@@ -5,20 +5,8 @@ import { POKEMON_FAVORITE_MUTATION, POKEMON_UNFAVORITE_MUTATION } from './mutati
 
 export const usePokemonFavoriteMutations = () => {
   // Pretending this is also a working optimistic ui implementation (haven't tested it)
-  const [setFavorite, { loading: loadingSet, error: errorSet }] = useMutation(
-    POKEMON_FAVORITE_MUTATION,
-    {
-      // Is there a better way to do this, e.g. enums? I couldn't interpolate the query name
-      // from an enum without getting errors since I'm autogenerating types
-      refetchQueries: ['GetPokemonList'],
-    },
-  )
-  const [setUnfavorite, { loading: loadingUnset, error: errorUnset }] = useMutation(
-    POKEMON_UNFAVORITE_MUTATION,
-    {
-      refetchQueries: ['GetPokemonList'],
-    },
-  )
+  const [setFavorite] = useMutation(POKEMON_FAVORITE_MUTATION)
+  const [setUnfavorite] = useMutation(POKEMON_UNFAVORITE_MUTATION)
 
   const exposedSet = (pokemonId: string) => {
     const promise = setFavorite({ variables: { pokemonId } })
